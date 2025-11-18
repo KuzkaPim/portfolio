c1f49cce04aa2401345529a59affafb7bd3ed6e6
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2025-11-18 17:40:03 +0500

    feature/updated-header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,27 +2,39 @@ import { Container } from '@/src/shared/ui';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 bg-white z-20 shadow-lg h-16 text-gray-800 text-lg">
+    <header className="sticky top-0 bg-primary z-20 shadow-sm h-16 text-gray-800 text-lg">
       <Container className="h-full flex items-center">
         <nav>
           <ul className="flex items-center gap-8">
             <li>
-              <a href="#promo" className="transition hover:border-b-2">
+              <a
+                href="#promo"
+                className="transition border-accent hover:border-b-2"
+              >
                 Главная
               </a>
             </li>
             <li>
-              <a href="#skills" className="transition hover:border-b-2">
+              <a
+                href="#skills"
+                className="transition border-accent hover:border-b-2"
+              >
                 Мой стек / технологии
               </a>
             </li>
             <li>
-              <a href="#about-me" className="transition hover:border-b-2">
+              <a
+                href="#about-me"
+                className="transition border-accent hover:border-b-2"
+              >
                 Обо мне
               </a>
             </li>
             <li>
-              <a href="#contacts" className="transition hover:border-b-2">
+              <a
+                href="#contacts"
+                className="transition border-accent hover:border-b-2"
+              >
                 Контакты
               </a>
             </li>
