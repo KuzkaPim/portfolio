2c0a557f867918bbad2b63293655e606fe777441
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2025-11-20 15:46:05 +0500

    fix/fixed-size-arrow-up

--- src/app/components/UpArrow.tsx
+++ src/app/components/UpArrow.tsx
@@ -29,7 +29,7 @@ export const UpArrow = () => {
   return (
     <FaArrowUp
       onClick={handleClick}
-      className={`bg-primary text-accent size-8 sm:size-10 w-max fixed right-3 sm:right-5 bottom-3 sm:bottom-5 p-1.5 sm:p-2 rounded-full shadow-md transition hover:scale-105 active:scale-100 ${isVisible ? 'scale-100' : 'scale-50 opacity-0 pointer-events-none'}`}
+      className={`bg-primary text-accent size-8 sm:size-10 fixed right-3 sm:right-5 bottom-3 sm:bottom-5 p-1.5 sm:p-2 rounded-full shadow-md transition hover:scale-105 active:scale-100 ${isVisible ? 'scale-100' : 'scale-50 opacity-0 pointer-events-none'}`}
     />
   );
 };
