d8b5fa96ee05369459c7182ca8b83e2a19b34183
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2025-12-16 17:11:39 +0500

    fix/removed-next-configs-for-react-pdf

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  transpilePackages: ['react-pdf', 'pdfjs-dist', 'next-intl', 'react-icons'],
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
