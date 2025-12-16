96fdc6ec1893a0ecbf252d374058e4079a41cb98
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2025-12-16 13:45:18 +0500

    fix/added-transpile-packages-for-react-pdf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  transpilePackages: ['react-pdf', 'pdfjs-dist'],
+};
 const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
