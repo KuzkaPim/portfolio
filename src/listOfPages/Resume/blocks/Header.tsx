8ac568eb40a8aa434f24da1ca48158d291a14337
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2026-01-20 15:14:58 +0500

    feature/updated-colors-for-back-button-in-resume-page

--- src/listOfPages/Resume/blocks/Header.tsx
+++ src/listOfPages/Resume/blocks/Header.tsx
@@ -8,10 +8,10 @@ export const Header = () => {
       <Container className="flex justify-between gap-4 items-center">
         <Link
           href="/"
-          className="bg-accent/80 backdrop-blur-md border border-white/20 size-11 rounded-full text-content-secondary p-1 pr-1.5 hover:bg-accent transition duration-200 group/back shadow-md"
+          className="bg-accent/60 backdrop-blur-sm border border-white/20 size-11 rounded-full text-content-secondary p-1 pr-1.5 hover:bg-accent/70 transition duration-250 group/back shadow-md"
           aria-label="Go back home"
         >
-          <MdKeyboardArrowLeft className="transition duration-200 size-full group-hover/back:-translate-x-0.5" />
+          <MdKeyboardArrowLeft className="transition duration-250 size-full group-hover/back:-translate-x-0.5" />
         </Link>
         <Swithes />
       </Container>
