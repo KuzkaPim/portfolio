c111d49bcef58cda6de617d8d0dcad4c8d4762a5
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2026-01-21 13:53:22 +0500

    feature/change-hex-to-hsl-format

--- src/listOfPages/Home/blocks/PromoBlock.tsx
+++ src/listOfPages/Home/blocks/PromoBlock.tsx
@@ -31,13 +31,13 @@ export const PromoBlock = () => {
             </Link>
             <div className="flex gap-4 flex-col sm:flex-row sm:w-full lg:w-auto">
               <a
-                className="px-4 py-2 border border-accent/10 dark:border-accent/30 bg-accent/10 dark:bg-accent/30 font-bold rounded-4xl text-accent dark:text-content-secondary transition sm:flex-2 lg:flex-initial text-center hover:bg-accent/20 dark:hover:bg-accent/40"
+                className="px-4 py-2 border border-accent/10 dark:border-accent/30 bg-accent/10 dark:bg-accent/30 font-bold rounded-4xl text-accent dark:text-navigation transition sm:flex-2 lg:flex-initial text-center hover:bg-accent/20 dark:hover:bg-accent/40"
                 href="#contacts"
               >
                 {t('btn.contactMe')}
               </a>
               <Link
-                className="px-4 py-2 border border-accent/10 dark:border-accent/30 bg-accent/10 dark:bg-accent/30 font-bold rounded-4xl text-accent dark:text-content-secondary transition sm:flex-1 lg:flex-initial text-center w-max hover:bg-accent/20 dark:hover:bg-accent/40"
+                className="px-4 py-2 border border-accent/10 dark:border-accent/30 bg-accent/10 dark:bg-accent/30 font-bold rounded-4xl text-accent dark:text-navigation transition sm:flex-1 lg:flex-initial text-center w-max hover:bg-accent/20 dark:hover:bg-accent/40"
                 href="/resume"
               >
                 {t('btn.resume')}
