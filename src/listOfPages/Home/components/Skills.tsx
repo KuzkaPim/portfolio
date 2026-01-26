afcb849364bdb66bcfd5b547ba14c983897cff39
Kuzma <88473760+KuzkaPim@users.noreply.github.com>
2026-01-26 15:54:38 +0500

    feature/updated-tracking-for-skills-block

--- src/listOfPages/Home/components/Skills.tsx
+++ src/listOfPages/Home/components/Skills.tsx
@@ -13,7 +13,7 @@ export const Skills = ({ catagory }: Props) => {
       {skills.map((skill) => (
         <li
           key={skill}
-          className="px-4 py-1 bg-secondary text-content-primary font-mono rounded-md shadow-sm shadow-layer/80 hover:-translate-y-1 transition cursor-default uppercase scale-y-125"
+          className="px-4 py-1 bg-secondary text-content-primary font-mono rounded-md shadow-sm shadow-layer/80 hover:-translate-y-1 transition cursor-default uppercase scale-y-125 tracking-[1.2px]"
         >
           {skill}
         </li>
